c0d8e8e755dded5c3db1cb38c75d079821ecba93
deepikaPonakala <23b01a42a2@svecw.edu.in>
2026-01-02 21:20:07 +0530

    Add files via upload

---
+++ from flask import Flask, render_template.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load trained model and feature columns
+rf_model = pickle.load(open("models/rf_model.pkl", "rb"))
+feature_columns = pickle.load(open("models/feature_columns.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    data = {
+        "CarName": request.form["CarName"],
+        "fueltype": request.form["fueltype"],
+        "aspiration": request.form["aspiration"],
+        "doornumber": request.form["doornumber"],
+        "carbody": request.form["carbody"],
+        "drivewheel": request.form["drivewheel"],
+        "enginelocation": request.form["enginelocation"],
+        "wheelbase": float(request.form["wheelbase"]),
+        "carlength": float(request.form["carlength"]),
+        "carwidth": float(request.form["carwidth"]),
+        "carheight": float(request.form["carheight"]),
+        "curbweight": float(request.form["curbweight"]),
+        "enginetype": request.form["enginetype"],
+        "cylindernumber": request.form["cylindernumber"],
+        "enginesize": float(request.form["enginesize"]),
+        "fuelsystem": request.form["fuelsystem"],
+        "boreratio": float(request.form["boreratio"]),
+        "stroke": float(request.form["stroke"]),
+        "compressionratio": float(request.form["compressionratio"]),
+        "horsepower": float(request.form["horsepower"]),
+        "peakrpm": float(request.form["peakrpm"]),
+        "citympg": float(request.form["citympg"]),
+        "highwaympg": float(request.form["highwaympg"])
+    }
+
+    # Convert input to DataFrame
+    df = pd.DataFrame([data])
+
+    # One-hot encode (SAME AS TRAINING)
+    df = pd.get_dummies(df, drop_first=True)
+
+    # Align with training columns
+    df = df.reindex(columns=feature_columns, fill_value=0)
+
+    prediction = round(rf_model.predict(df)[0], 2)
+
+    return render_template("index.html", prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
